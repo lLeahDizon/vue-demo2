4a8eea1c882d86123dc3ec42aaa1121f6ec1cb0d
Lemon <516330797@qq.com>
2020-12-12 06:57:11 +0800

    测试v-modell

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div id="app">
+    {{ user }}
+    <hr />
     登录
     <hr />
     <form @submit.prevent="onSubmit">
       <label>
         <span>用户名</span>
-        <input type="text" v-model="user.username" />
+        <my-input v-model="user.username" />
       </label>
       <label>
         <span>密码</span>
@@ -17,18 +19,19 @@
 </template>
 
 <script>
+import MyInput from "./MyInput.vue";
 export default {
   name: "App",
   data() {
     return {
       user: {
-        username: "",
+        username: 0,
         password: "",
       },
       x: [],
     };
   },
-  components: {},
+  components: { MyInput },
   methods: {
     onSubmit() {
       console.log(this.user);
