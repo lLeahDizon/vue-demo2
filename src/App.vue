2315b2b9b3f55dae549913b7e169a15178061403
Lemon <516330797@qq.com>
2020-12-12 07:49:13 +0800

    引入ant design vue

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,74 @@
 <template>
   <div id="app">
-    {{ user }}
-    <hr />
-    登录
-    <hr />
-    <form @submit.prevent="onSubmit">
-      <label>
-        <span>用户名</span>
-        <my-input v-model="user.username" />
-      </label>
-      <label>
-        <span>密码</span>
-        <input type="password" v-model="user.password" />
-      </label>
-      <button type="submit">登录</button>
-    </form>
+    <a-form
+      id="components-form-demo-normal-login"
+      :form="form"
+      class="login-form"
+      @submit.prevent="onSubmit"
+    >
+      <a-form-item>
+        <a-input
+          v-decorator="[
+            'username',
+            { rules: [{ required: true, message: '没写 username' }] },
+          ]"
+          placeholder="用户名"
+        >
+          <a-icon slot="prefix" type="user" style="color: rgba(0,0,0,.25)" />
+        </a-input>
+      </a-form-item>
+      <a-form-item>
+        <a-input
+          v-decorator="[
+            'password',
+            {
+              rules: [
+                { required: true, message: '没填密码' },
+                { min: 8, message: '密码最少8个字符' },
+                { pattern: /[a-zA-Z]/, message: '必须包含至少一个字母' },
+              ],
+            },
+          ]"
+          type="password"
+          placeholder="密码"
+        >
+          <a-icon slot="prefix" type="lock" style="color: rgba(0,0,0,.25)" />
+        </a-input>
+      </a-form-item>
+      <a-button type="primary" html-type="submit" class="login-form-button">
+        Log in
+      </a-button>
+    </a-form>
   </div>
 </template>
 
 <script>
-import MyInput from "./MyInput.vue";
 export default {
   name: "App",
-  data() {
-    return {
-      user: {
-        username: 0,
-        password: "",
-      },
-      x: [],
-    };
+  beforeCreate() {
+    this.form = this.$form.createForm(this, { name: "normal_login" });
   },
-  components: { MyInput },
   methods: {
     onSubmit() {
-      console.log(this.user);
+      this.form.validateFields((err, values) => {
+        if (!err) {
+          console.log("Received values of form: ", values);
+        } else {
+          console.log(err);
+        }
+      });
     },
   },
 };
 </script>
+<style>
+#components-form-demo-normal-login .login-form {
+  max-width: 300px;
+}
+#components-form-demo-normal-login .login-form-forgot {
+  float: right;
+}
+#components-form-demo-normal-login .login-form-button {
+  width: 100%;
+}
+</style>
